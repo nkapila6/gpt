df910b3f46049135ecf123b3ce540cdc70482079
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-04-06 19:40:26 +0400

    feat(tokenizer.py): added a Vocab class

--- tokenizer.py
+++ tokenizer.py
@@ -7,6 +7,38 @@ import re
 from utils import read_data
 
 
+class Vocab:
+    def __init__(self, text: str):
+        self.special_tokens = ["<unk>", "<bos>", "<eos>"]
+
+        words = re.split(r'([,.:;?_!"()\']|--|--|\s)', text)
+        words = [w for w in words if w and not w.isspace()]
+
+        self.tokens = sorted(set(words) - set(self.special_tokens))
+        self.tokens.extend(self.special_tokens)  # adding special tokens
+        self.tokens = sorted(set(words))  # tokens is now a list
+
+        # adding vocab len
+        self.vocab = {w: id for id, w in enumerate(self.tokens)}
+        self.inv_vocab = {id: w for id, w in enumerate(self.tokens)}
+        self.vocab_len = len(self.tokens)
+
+    def __len__(self):
+        # length
+        return self.vocab_len
+
+    def update(self, text: str):
+        # update with new data
+        words = re.split(r'([,.:;?_!"()\']|--|--|\s)', text)
+        words = [w for w in words if w and not w.isspace()]
+        new_words = sorted(set(words) - set(self.tokens))
+
+        self.tokens.extend(new_words)
+        self.vocab = {w: idx for idx, w in enumerate(self.tokens)}
+        self.inv_vocab = {id: w for id, w in enumerate(self.tokens)}
+        self.vocab_len = len(self.tokens)
+
+
 def word_level_tokenizer(path: str) -> tuple[list, dict, dict]:
     data = read_data(path)
 
