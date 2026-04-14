3c2375b2f4610fdb48d34f502dac0887492c2b96
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-04-14 13:15:07 +0400

    fix(config.py): limiting seqlen for local training

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     emb_size: int = 768  # embedding size
     n_layers: int = 12  # number of transformer blocks
     num_heads: int = 12  # number of heads for multihead att
-    seq_len: int = 512  # context length
+    seq_len: int = 128  # context length
     vocab_size: int = 500  # vocab size, TODO: base it on required
     dropout: float = 0.5  # dropout
     out_proj: int = 3072  # linear projection
